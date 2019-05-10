6658e772a43b816ea1d93d31aa19f4a62f61bfef
Taiping Li <litaiping97@gmail.com>
2019-05-10 11:38:35 -0400

    Added PB_8 to the list of pins

--- stm32f072-serial-control/main.cpp
+++ stm32f072-serial-control/main.cpp
@@ -50,13 +50,14 @@ struct long_data {
 }; 
 
 // Define all pins on the STM32F072CBT6 48QFP 
-const int GPIO_PIN_COUNT = 31; 
+const int GPIO_PIN_COUNT = 32; 
 const PinName GPIO[] = {
     PA_0,  PA_1,  PA_2,  PA_3,
     PA_4,  PA_5,  PA_6,  PA_7,
     PA_8,  PA_9,  PA_10, PA_15,
     PB_0,  PB_1,  PB_2,  PB_3,
-    PB_4,  PB_5,  PB_6,  PB_7,
+    PB_4,  PB_5,  PB_6,  PB_7, 
+    PB_8,
     PB_9,  PB_10, PB_11, PB_12,
     PB_13, PB_14, PB_15, PC_13,
     ADC_TEMP, ADC_VREF, ADC_VBAT
